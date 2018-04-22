e77e122ac87dd56ad9859f8769352de3f8825957
Guido van de Riet <guidovdriet@gmail.com>
2018-04-22 20:57:32 +0200

    Add socket connection

--- App.js
+++ App.js
@@ -8,24 +8,40 @@ import Account from "./components/Account";
 import Packages from "./components/Packages";
 import Scan from "./components/Scan";
 
+const socketClient = io("http://45.77.159.108:7000");
+
+const mapSocketClientToNavigation = Component => {
+  return class extends Component {
+    render() {
+      const { navigation, ...other } = this.props;
+      const {
+        state: { params }
+      } = navigation;
+      return (
+        <Component {...this.props} {...params} socketClient={socketClient} />
+      );
+    }
+  };
+};
+
 const App = TabNavigator(
   {
     Packages: {
-      screen: Packages,
+      screen: mapSocketClientToNavigation(Packages),
       navigationOptions: {
         tabBarIcon: <Icon name="package-down" type="material-community" color="#1FB5FC" />,
         tabBarLabel: "Packages"
       }
     },
     Scan: {
-      screen: Scan,
+      screen: mapSocketClientToNavigation(Scan),
       navigationOptions: {
         tabBarIcon: <Icon reverse name="qrcode-scan" type="material-community" color="#1FB5FC" />,
         tabBarLabel: " "
       }
     },
     Account: {
-      screen: Account,
+      screen: mapSocketClientToNavigation(Account),
       navigationOptions: {
         tabBarIcon: <Icon name="account" type="material-community" color="#1FB5FC" />,
         tabBarLabel: "Account"
