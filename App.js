603043c99fc9057482234cc868a4580749faf0d2
Guido van de Riet <guidovdriet@gmail.com>
2018-04-22 18:31:36 +0200

    Add list of packages by name

--- App.js
+++ App.js
@@ -8,9 +8,23 @@ import Confirmation from "./components/Confirmation";
 
 const socketClient = io("http://45.77.159.108:7000");
 
+const mapSocketClientToNavigation = Component => {
+  return class extends Component {
+    render() {
+      const { navigation, ...other } = this.props;
+      const {
+        state: { params }
+      } = navigation;
+      return (
+        <Component {...this.props} {...params} socketClient={socketClient} />
+      );
+    }
+  };
+};
+
 const App = StackNavigator({
-  Home: { screen: PackagesOverview },
-  Confirm: { screen: Confirmation }
+  Home: { screen: mapSocketClientToNavigation(PackagesOverview) },
+  Confirm: { screen: mapSocketClientToNavigation(Confirmation) }
 });
 
 export default () => <App />;
--- components/PackagesOverview/index.js
+++ components/PackagesOverview/index.js
@@ -1,33 +1,45 @@
 import React, { Component } from "react";
 import { View, Text, FlatList } from "react-native";
+import styled from "styled-components";
+import { List, ListItem } from "react-native-elements";
 
 class PackagesOverview extends Component {
+  _sendNotification(key) {
+    this.props.socketClient.emit("delivery:change-home-notification", {
+      packageId: key
+    });
+  }
+
   render() {
     return (
-      <FlatList
-        data={[
-          {
-            key: "a",
-            name: "Karel Appelman",
-            adress: "Bergweg 127A",
-            note: "bla bla bla bla bla"
-          },
-          {
-            key: "b",
-            name: "Jan de Groot",
-            adress: "Bergweg 127B",
-            note: "bla bla bla bla bla"
-          }
-        ]}
-        renderItem={({ item }) => 
-            <View>
-                <Text>{item.key}</Text>
-                <Text>{item.name}</Text>
-                <Text>{item.adress}</Text>
-                <Text>{item.note}</Text>
-            </View>
-        }
-      />
+      <List>
+        <FlatList
+          data={[
+            {
+              key: "a",
+              name: "Karel Appelman",
+              address: "Bergweg 127A",
+              note: "bla bla bla bla bla"
+            },
+            {
+              key: "b",
+              name: "Jan de Groot",
+              address: "Bergweg 127B",
+              note: "bla bla bla bla bla"
+            }
+          ]}
+          renderItem={({ item }) => (
+            <ListItem
+              onPress={() => this._sendNotification(item.key)}
+              underlayColor="#7CE065"
+              key={item.key}
+              title={item.name}
+              address={item.address}
+              note={item.note}
+            />
+          )}
+        />
+      </List>
     );
   }
 }
