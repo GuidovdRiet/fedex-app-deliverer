e18ba9b7ddd22e22a5c94399218c4d3da3d90a97
Guido van de Riet <guidovdriet@gmail.com>
2018-05-29 12:19:02 +0200

    Add QR code scanner

--- components/Scan/index.js
+++ components/Scan/index.js
@@ -1,72 +1,52 @@
 "use strict";
+
 import React, { Component } from "react";
+
 import {
   AppRegistry,
-  Dimensions,
   StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
+  NavigatorIOS,
+  Dimensions
 } from "react-native";
-import { RNCamera } from "react-native-camera";
 
-export default class Scan extends Component {
-  takePicture = async function() {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
-  };
+import QRCodeScanner from "react-native-qrcode-scanner";
+
+class Scan extends Component {
+  onSuccess(e) {
+    console.log(e.data);
+    // Linking.openURL(e.data).catch(err =>
+    //   console.error("An error occured", err)
+    // );
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          permissionDialogTitle={"Permission to use camera"}
-          permissionDialogMessage={
-            "We need your permission to use your camera phone"
+      <NavigatorIOS
+        initialRoute={{
+          component: QRCodeScanner,
+          title: "Scan Package QR",
+          passProps: {
+            onRead: this.onSuccess.bind(this),
+            cameraStyle: styles.cameraContainer,
+            topViewStyle: styles.zeroContainer,
+            bottomViewStyle: styles.zeroContainer
           }
-        />
-        <View
-          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
-        >
-          <TouchableOpacity
-            onPress={this.takePicture.bind(this)}
-            style={styles.capture}
-          >
-            <Text style={{ fontSize: 14 }}> SNAP </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+        }}
+        style={{ flex: 1 }}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "black"
+  zeroContainer: {
+    height: 0,
+    flex: 0
   },
-  preview: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center"
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: "center",
-    margin: 20
+
+  cameraContainer: {
+    height: Dimensions.get("window").height
   }
 });
+
+export default Scan;
